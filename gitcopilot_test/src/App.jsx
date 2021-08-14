179f02055b2758c2fbfedd7bb257aab468f3cb47
Your Name <71481611+Virajj28@users.noreply.github.com>
2021-08-14 10:11:58 +0530

    export

--- gitcopilot_test/src/App.jsx
+++ gitcopilot_test/src/App.jsx
@@ -1,18 +1,12 @@
 import './App.css';
-import {useEffect, useState} from 'react';
-
+import {Templates} from './Templates';
 
 function App() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-  }
-  , []);
+  
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
-      <p>Github Copilot is real cool</p>
-       
-          </div>
+      <Templates/>
+      </div>
   );
 }
 
---
+++ gitcopilot_test/src/Templates.jsx
@@ -0,0 +1,14 @@
+
+function Templates (props) {
+  return (
+    <div>
+      <h1>Templates</h1>
+      <p>
+        This is a test template.
+      </p>
+    </div>
+  );
+}
+
+export default Templates;
+           
